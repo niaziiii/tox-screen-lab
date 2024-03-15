7d59a58b385be48f05dd02ff3c0189b31e56f0aa
Fatima Ali <fatimaaliforwork@gmail.com>
2024-03-15 09:59:25 +0500

    Adds FAQ and test details UI

---
+++ src/common/FAQ.tsx
@@ -0,0 +1,89 @@
+import { Accordion, AccordionTab } from "primereact/accordion";
+
+// interface AccordionProps {
+//   header: string;
+//   content: string;
+// }
+
+const FAQ = () => {
+  return (
+    <>
+      <h2 className="text-center text-3xl text-dark font-bold">FAQ</h2>
+      <Accordion multiple activeIndex={[0]} className="flex flex-col gap-10">
+        <AccordionTab
+          pt={{
+            headerIcon: {
+              className: " absolute right-0 h-6 w-6 text-darkBlue",
+            },
+            header: {
+              className: "h-16  text-darkBlue text-xl font-medium",
+            },
+          }}
+          header="What screenings should men get?"
+        >
+          <p className="mt-10 text-base text-dark">
+            {
+              "Our Men’s Health Test includes standard tests for men’s wellness including a comprehensive metabolic panel (CMP) blood test, a complete blood count (CBC) test, a diabetes risk (HbA1c) test, a lipid panel, and a urine analysis test that can screen for various conditions, such as kidney disease or diabetes. Having regular screenings improves the likelihood of early detection of a variety of health issues, ultimately improving diagnoses and treatment options."
+            }
+          </p>
+        </AccordionTab>
+
+        <AccordionTab
+          pt={{
+            headerIcon: {
+              className: " absolute right-0 h-6 w-6 text-darkBlue",
+            },
+            header: {
+              className: "h-fit text-darkBlue text-xl font-medium",
+            },
+          }}
+          header="How accurate is this testing?"
+        >
+          <p className="mt-10 text-base text-dark">
+            {
+              "Our Men’s Health Test includes standard tests for men’s wellness including a comprehensive metabolic panel (CMP) blood test, a complete blood count (CBC) test, a diabetes risk (HbA1c) test, a lipid panel, and a urine analysis test that can screen for various conditions, such as kidney disease or diabetes. Having regular screenings improves the likelihood of early detection of a variety of health issues, ultimately improving diagnoses and treatment options."
+            }
+          </p>
+        </AccordionTab>
+
+        <AccordionTab
+          pt={{
+            headerIcon: {
+              className: " absolute right-0 h-6 w-6 text-darkBlue",
+            },
+            header: {
+              className: "h-10  text-darkBlue text-xl font-medium",
+            },
+          }}
+          header="Are my test results private and confidential?"
+        >
+          <p className="mt-10 text-base text-dark">
+            {
+              "Our Men’s Health Test includes standard tests for men’s wellness including a comprehensive metabolic panel (CMP) blood test, a complete blood count (CBC) test, a diabetes risk (HbA1c) test, a lipid panel, and a urine analysis test that can screen for various conditions, such as kidney disease or diabetes. Having regular screenings improves the likelihood of early detection of a variety of health issues, ultimately improving diagnoses and treatment options."
+            }
+          </p>
+        </AccordionTab>
+
+        <AccordionTab
+          pt={{
+            headerIcon: {
+              className: " absolute right-0 h-6 w-6 text-darkBlue",
+            },
+            header: {
+              className: "h-10  text-darkBlue text-xl font-medium",
+            },
+          }}
+          header="How does TOX-SCREEN LAB work?"
+        >
+          <p className="mt-10 text-base text-dark">
+            {
+              "Our Men’s Health Test includes standard tests for men’s wellness including a comprehensive metabolic panel (CMP) blood test, a complete blood count (CBC) test, a diabetes risk (HbA1c) test, a lipid panel, and a urine analysis test that can screen for various conditions, such as kidney disease or diabetes. Having regular screenings improves the likelihood of early detection of a variety of health issues, ultimately improving diagnoses and treatment options."
+            }
+          </p>
+        </AccordionTab>
+      </Accordion>
+    </>
+  );
+};
+
+export default FAQ;
--- src/pages/TestDetail/index.tsx
+++ src/pages/TestDetail/index.tsx
@@ -1,5 +1,58 @@
+import { Button } from "primereact/button";
+import TestDetailImage from "../../assets/testcard.png";
+import FAQ from "../../common/FAQ";
 const TestDetail = () => {
-  return <div>TestDetail</div>;
+  return (
+    <div className=" w-[85%]  mx-auto mt-24 mb-64 flex flex-col gap-28 ">
+      <article className="max-w-sm mx-auto md:max-w-none grid md:grid-cols-2 gap-6 md:gap-8 lg:gap-12 xl:gap-16 items-center">
+        <a
+          className="relative  group flex items-start justify-start  h-full"
+          href="#"
+        >
+          <img
+            className=" w-full h-full object-cover rounded-lg "
+            src={TestDetailImage}
+            width="540"
+            height="303"
+            alt="Blog post"
+          />
+        </a>
+        <div className=" h-full flex flex-col gap-8">
+          <header className="">
+            <div className="mb-3">
+              <h1 className="text-3xl font-bold text-dark">
+                Comprehensive Health Profile — Men's
+              </h1>
+            </div>
+            <h2 className="text-xl lg:text-xl text-lightBlue font-bold leading-tight mb-2">
+              $349.00
+            </h2>
+          </header>
+          <div className="flex flex-col gap-3">
+            <h3 className="text-xl text-darkBlue font-semibold">Details</h3>
+            <p className="text-lg text-dark  flex-grow">
+              The tests your healthcare provider recommends for an annual health
+              check are now at your fingertips. From hormone testing for men to
+              screening for diabetes, heart disease and more—you’ll get the
+              insights you need to work towards a healthier future.
+            </p>
+          </div>
+
+          <footer className="flex mt-4">
+            <Button
+              label="Purchase"
+              className=" bg-lightBlue text-white px-20 py-4 text-lg rounded-lg hover:bg-darkBlue duration-300 ease-in transition-all"
+              size="large"
+            />
+          </footer>
+        </div>
+      </article>
+
+      <section className="md:w-8/12 mx-auto flex flex-col gap-16">
+        <FAQ />
+      </section>
+    </div>
+  );
 };
 
 export default TestDetail;
